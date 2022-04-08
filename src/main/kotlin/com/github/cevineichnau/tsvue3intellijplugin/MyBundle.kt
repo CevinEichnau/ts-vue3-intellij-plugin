c34707a6e49ccaaaf2a8ff074bb2e897b322c3e8
GitHub Action <action@github.com>
2022-04-08 09:06:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/cevineichnau/tsvue3intellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/cevineichnau/tsvue3intellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cevineichnau.tsvue3intellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/cevineichnau/tsvue3intellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cevineichnau.tsvue3intellijplugin.services
+
+import com.github.cevineichnau.tsvue3intellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
